d86366e2ceea2051ce1ce92b02b7ba92163c82ce
MartinDelille <martin@delille.org>
2021-11-02 10:23:13 +0000

    Deploying to gh-pages from @ le-lerot/molky@1bb1db12f2d911c84a26e0bb644bc687e06b084a 🚀

--- src/components/Game/Game.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Game.module.css';
-
-const Game = ({ currentPlayer }) => {
-    return <div className={classes.Game}>{currentPlayer}</div>;
-};
-
-export default Game;
