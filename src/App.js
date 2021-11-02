ed1331c9e4b51ba60aa42e25818a600abddb3bae
Martin Delille <martin@delille.org>
2021-11-02 15:04:52 +0100

    Remove read.development in import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Board from './components/Board/Board';
 import Game from './components/Game/Game';
 import classes from './App.module.css';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 //import { shuffleArray } from './utils/helpers';
 
 const players = [
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import Button from '../Button/Button';
 import Score from '../Score/Score';
 import WinnerBanner from '../WinnerBanner/WinnerBanner';
